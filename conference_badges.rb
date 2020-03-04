4dd721ed746c4ee885766e721128dcdfa00a3ce2
Xiaoyi Liu <lxy878@gmail.com>
2020-03-04 09:24:35 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.each do |name|
+    badges << badge_maker(name)
+  end
+  return badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  room = 1
+  attendees.each do |name|
+    room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+    room += 1
+  end
+  return room_assignments
+end
+
+def printer(attendees)
+  badge = batch_badge_creator(attendees)
+  assign_rooms(attendees)
+  l = attendees.length
+
+end
